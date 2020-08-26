4359c612f37fc68ae0c67027e767efe3b0118c02
Bruno Ruy <brunoruy79@gmail.com>
2020-08-26 11:24:24 -0300

    Terminado questão 4 (falta análise para redução de linhas)

--- Questão 04.py
+++ Questão 04.py
@@ -22,12 +22,65 @@ def pad2(n):
                 print(' '*(n-i), '# #')
             else:
                 print(' '*(n-i), '#', ' '*(1+2*(i-1)-2), '#')
-def pad3():
-    r = ''
-    r = (' ' * 5 + '_\n' + ' ' * 3 + '_( )_\n' + ' ' * 1 + '_|     _|\n' + ' ' * 0 + '(_   _ (_\n' + ' ' * 2 + '|_(  )_|')
-    return r
-
-print(pad3())
+def pad3(n):
+    for k in range (n):
+        if k == 0:
+            for i in range(n):
+                print(' '*5 + '_ ', end= '')
+            print('\n   ', end = '')
+            for i in range(n):
+                print('_( )__' + ' ',end = '')
+                if i == n-1:
+                    print('')
+            for i in range (n+1):
+                print(' ' + '_|' + ' '*4, end = '')
+                if i == n:
+                    print('')
+            print('(_' + ' '*3, end = '')
+            for i in range(n):
+                print('_ ' + '(_ ', end = '  ')
+                if i == n-1:
+                    print('')
+            print(' |', end = '')
+            for i in range(n):
+                print('__( )_', end= '|')
+                if i == n-1:
+                    print('')
+        elif k % 2 != 0:
+            for i in range(n):
+                if i == 0:
+                    print(' |_', end = '')
+                print('     |_', end = '')
+                if i == n -1:
+                    print('')
+            for i in range(n):
+                if i == 0:
+                    print('  _)', end = '')
+                print(' _   _)', end = '')
+                if i == n-1:
+                    print('')
+            for i in range (n):
+                if i ==0:
+                    print(' |', end = '')
+                print('__( )_|', end='')
+                if i == n-1:
+                    print('')
+        elif k % 2 == 0:
+            for i in range (n+1):
+                print(' ' + '_|' + ' '*4, end = '')
+                if i == n:
+                    print('')
+            print('(_' + ' '*3, end = '')
+            for i in range(n):
+                print('_ ' + '(_ ', end = '  ')
+                if i == n-1:
+                    print('')
+            print(' |', end = '')
+            for i in range(n):
+                print('__( )_', end= '|')
+                if i == n-1:
+                    print('')
 n = int(input())
 print(pad1(n))
 pad2(n)
+pad3(n)
