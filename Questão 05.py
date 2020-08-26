9affdfb318c00e00ac610b7b73c6c4eb2ec94a53
Bruno Ruy <brunoruy79@gmail.com>
2020-08-26 16:13:55 -0300

    corrigido a questão 5, após explicação do funcionamento das regras

--- Questão 05.py
+++ Questão 05.py
@@ -1,22 +1,53 @@
-def jogobarra(n):
+def regra1(n):
     a = n
-    k = 0
-    if a > 42:
-        if n % 2 == 0:
-            a = n/2
-        elif n % 3 == 0 or n % 4 == 0:
-            a = n - (n%10) * ((n // 10) % 10)
-        elif n % 5 == 0:
-            a = n - 42
-        else:
-            k = 1
-        if k != 1:
-            jogobarra((a))
-    if a == 42:
-        res = True
+    if n % 2 == 0:
+        a = n / 2
+    return a
+def regra2(n):
+    a = n
+    if n % 3 == 0 or n % 4 == 0:
+        a = n - (n % 10) * ((n // 10) % 10)
+    return a
+def regra3(n):
+    a = n
+    if n % 5 == 0:
+        a = n - 42
+    return a
+def jogobarra(n):
+    c = 0
+    t1 = t2 = t3 = True
+    if n == 42:
+        return True
+    if regra1(n) != n:
+        c += 1
     else:
-        res= False
-    return res
+        t1 = False
+    if regra2(n) != n:
+        c += 1
+    else:
+        t2 = False
+    if regra3(n) != n:
+        c += 1
+    else:
+        t3 = False
+    for i in range (c):
+        if regra1(n) != n and i == 0:
+            a = regra1(n)
+            t1 = jogobarra(a)
+            if t1 == True:
+                return True
+        elif (regra2(n) != n or (t1 == False and regra2(n) != n)) and t2 != False :
+            a = regra2(n)
+            t2 = jogobarra(a)
+            if t2 == True:
+                return True
+        elif (regra3(n) != n  or (t1 == False and regra3(n) != n) or (t2 == False and regra2(n) != n)) and t3 != False:
+            a = regra3(n)
+            t3 = jogobarra(a)
+            if t3 == True:
+                return True
+    if t1 == t2 == t3 == False or n < 42:
+        return False
 n = int(input())
 if jogobarra(n) == True:
     print('Venci')
